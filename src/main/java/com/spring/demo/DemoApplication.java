b22f20a6260da9e050a6f50319f627637adfe355
Anton Marklund <antonmarklund@MacBook-Pro-som-tillhor-Anton.local>
2020-09-15 20:35:07 +0200

    Angular and spring boot

---
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	
+	@RequestMapping("greeting")
+	public String asd () {
+		return "hello";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
